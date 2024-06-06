353edec81e4c4d8077e6c7cd0db41ec16909850a
haoxingxing <mr.haoxx@gmail.com>
2024-06-06 16:21:10 +0800

    opt

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,40 @@
 #include "nn.hpp"
 
 #include <iostream>
+#include <vector>
 
 int main()
 {
     DataLoader<uint8_t, 28, 28, true> loader("../data/train-images-idx3-ubyte", "../data/train-labels-idx1-ubyte");
     DataLoader<uint8_t, 28, 28, true> loader_test("../data/t10k-images-idx3-ubyte", "../data/t10k-labels-idx1-ubyte");
 
+    std::vector<Matrix<double, 1, 784>> images_train;
+    std::vector<Matrix<double, 1, 10>> labels_train;
+
+    std::vector<Matrix<double, 1, 784>> images_test;
+    std::vector<uint8_t> labels_test;
+
+    while (!loader.eof())
+    {
+        auto [imgs, labels] = loader.read<1000>();
+        for (int i = 0; i < 1000; i++)
+        {
+            images_train.push_back(Image<uint8_t, 28, 28>(imgs[i]).reshape<1, 784>().scale(1.0 / 255.0));
+            labels_train.push_back(nn::function::onehot<double, 1, 10>(Matrix<double, 1, 1>({double(labels[i])})));
+        }
+    }
+
+    while (!loader_test.eof())
+    {
+        auto [imgs, labels] = loader_test.read<1000>();
+        for (int i = 0; i < 1000; i++)
+        {
+            images_test.push_back(Image<uint8_t, 28, 28>(imgs[i]).reshape<1, 784>().scale(1.0 / 255.0));
+            labels_test.push_back(labels[i]);
+        }
+    }
+
+    std::print("Loaded {} images\n", images_train.size());
     srand(42);
 
     Linear l1(random<double, 784, 128>(), random<double, 1, 128>());
@@ -22,7 +50,7 @@ int main()
     Softmax<double, 1, 10> s1;
 
     CrossEntropy<double, 1, 10> ce;
-    
+
     Sequential seq(l1, r1, d1, l2, s1);
 
     for (int step = 0; step < 100; step++)
@@ -32,79 +60,51 @@ int main()
         double total_loss = 0;
         int total_train = 0;
 
-        while (!loader.eof())
+        for (int i = 0; i < images_train.size(); i++)
         {
-            auto [imgs, labels] = loader.read<1000>();
 
-            for (int i = 0; i < 1000; i++)
-            {
+            auto image = images_train[i];
+            auto label = labels_train[i];
+            l1.zero_grad();
+            l2.zero_grad();
 
-                auto img = Image<uint8_t, 28, 28>(imgs[i]);
-                auto p = img.reshape<1, 784>().scale(1.0 / 255.0);
+            auto act = seq.forward(image);
+            auto loss = ce.forward(act, label);
 
-                auto b = labels[i];
-                auto label = nn::function::onehot<double, 1, 10>(Matrix<double, 1, 1>({double(b)}));
-                l1.zero_grad();
-                l2.zero_grad();
+            seq.backward(ce.backward(act, label));
 
-                auto act = seq.forward(p);
-                auto loss = ce.forward(act, label);
+            l1.step(0.001);
+            l2.step(0.001);
 
-                auto dloss = ce.backward(act, label);
-                seq.backward(dloss);
+            total_loss += loss;
+            total_train++;
 
-                l1.step(0.001);
-                l2.step(0.001);
-       
-                total_loss += loss;
-                total_train++;
-
-                // std::cout << dloss << dl1 << std::endl;
-                // std::cout << img << int(b) << std::endl;
-                // std::cout << l1r <<act << label;
-
-           
-            }
+            // std::cout << dloss << dl1 << std::endl;
+            // std::cout << img << int(b) << std::endl;
+            // std::cout << l1r <<act << label;
         }
 
-        std::cout << "Step " << step << " Loss " << total_loss / total_train  << std::endl;
+        std::cout << "Step " << step << " Loss " << total_loss / total_train << std::endl;
 
         loader_test.reset();
         int correct = 0, total = 0;
 
-        while (!loader_test.eof())
+        for (int i = 0; i < images_test.size(); i++)
         {
-            auto [imgs, labels] = loader_test.read<100>();
+            auto image = images_test[i];
+            auto label = labels_test[i];
 
-            for (int i = 0; i < 100; i++)
+            auto act = seq.forward(image);
+
+            auto max_idx = std::distance(act.data.begin(), std::max_element(act.data.begin(), act.data.end()));
+
+            if (max_idx == label)
             {
-
-                auto img = Image<uint8_t, 28, 28>(imgs[i]);
-                auto p = img.reshape<1, 784>().scale(1.0 / 255.0);
-
-                auto b = labels[i];
-                auto act = seq.forward(p);
-
-                auto max = act.data[0];
-                int max_idx = 0;
-                for (int j = 1; j < 10; j++)
-                {
-                    if (act.data[j] > max)
-                    {
-                        max = act.data[j];
-                        max_idx = j;
-                    }
-                }
-
-                if (max_idx == b)
-                {
-                    correct++;
-                }
-                total++;
+                correct++;
             }
+            total++;
         }
 
-        
         std::cout << "verify " << correct << "/" << total << "  " << correct / float(total) << std::endl;
     }
     return 0;
